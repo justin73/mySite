9de31fa0306150274ee97a0e45f44b18dc9c743e
justin73 <jiameng_73@hotmail.com>
2020-10-18 23:30:07 -0400

    Updates

---
+++ precache-manifest.bcb2a4fd7e5d3ae1823e07d0c8c4f874.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a30d65b8096a3114d3d7bbd04959f04",
+    "url": "/mySite/index.html"
+  },
+  {
+    "revision": "bc54447d6cf056244755",
+    "url": "/mySite/static/css/main.a4e4d1a0.chunk.css"
+  },
+  {
+    "revision": "ef5e0325b16b6c7481c8",
+    "url": "/mySite/static/js/2.3a1b38c4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mySite/static/js/2.3a1b38c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc54447d6cf056244755",
+    "url": "/mySite/static/js/main.d457874a.chunk.js"
+  },
+  {
+    "revision": "9381dfc539f1333efbd0",
+    "url": "/mySite/static/js/runtime-main.8abf2596.js"
+  },
+  {
+    "revision": "09e7d0d210489a85bcef7a9bb870c03f",
+    "url": "/mySite/static/media/LovedbytheKing-Regular.09e7d0d2.ttf"
+  },
+  {
+    "revision": "eeca044ef8fa4525d3923045f1adbd6c",
+    "url": "/mySite/static/media/ZhiMangXing-Regular.eeca044e.ttf"
+  }
+]);
